7fa40eff45d95678d372980d4515b4ce4b1f776f
chrislkl <christopher.lew916@gmail.com>
2025-05-21 16:10:32 -0400

    fixing https again

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://gittimeai.vercel.app", "http://localhost:3000"],
+    allow_origins=["https://gittimeai.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
